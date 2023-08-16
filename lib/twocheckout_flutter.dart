a083ec699b7d6c35c2bb8c910adea56ffa004335
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-08-16 12:17:42 +0500

    feat: implement credentials in flutter plugin

--- lib/twocheckout_flutter.dart
+++ lib/twocheckout_flutter.dart
@@ -1,8 +1,15 @@
 
+import 'package:flutter/services.dart';
+
 import 'twocheckout_flutter_platform_interface.dart';
 
 class TwocheckoutFlutter {
   Future<String?> showPaymentMethods() {
     return TwocheckoutFlutterPlatform.instance.showPaymentMethods();
+  }
+  MethodChannel getMethodChannel() => TwocheckoutFlutterPlatform.instance.getMethodChannel();
+
+  setTwoCheckoutCredentials(String secretKey, String merchantKey){
+    TwocheckoutFlutterPlatform.instance.setTwoCheckCredentials(secretKey, merchantKey);
   }
 }
