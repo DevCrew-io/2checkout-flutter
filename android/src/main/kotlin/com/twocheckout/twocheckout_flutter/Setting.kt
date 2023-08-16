b7c8822b7b3c0964710bdfee02f167ed824421df
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-08-16 12:14:46 +0500

    feat: create setting module

---
+++ android/src/main/kotlin/com/twocheckout/twocheckout_flutter/Setting.kt
@@ -0,0 +1,8 @@
+package com.twocheckout.twocheckout_flutter
+
+class Setting {
+    companion object {
+        var merchantCode="254558257678"
+        var secretKey = "GksS#rw&n_X^gv3xLBNU"
+    }
+}
